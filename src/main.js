7fad02ecda51f54d3e3fae52cbd2e70e460835b6
ziya <ziyawang0806@gmail.com>
2024-03-28 16:54:14 +0800

    圖片懶加載

--- src/main.js
+++ src/main.js
@@ -7,9 +7,24 @@ import router from './router'
 // 初始css樣式
 import '@/styles/common.scss'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy',{
+    mounted(el,binding){
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+              if(isIntersecting){
+                el.src = binding.value
+              }
+            },
+          )
+    }
+})
