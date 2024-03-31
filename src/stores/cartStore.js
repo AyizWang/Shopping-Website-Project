35d2a4ed20735834bcb5e3a67504aa8426440537
ziya <ziyawang0806@gmail.com>
2024-03-31 11:40:26 +0800

    全選功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -18,12 +18,26 @@ export const useCartStore = defineStore('cart', () => {
     const allCount = computed(() => cartList.value.reduce((prev, item) => prev + item.count, 0))
     const allPrice = computed(() => cartList.value.reduce((prev, item) => prev + item.count * item.price, 0))
 
+    //單選功能
+    const singleCheck = (skuId, selected) => {
+        const item = cartList.value.find((item) => item.skuId === skuId)
+        item.selected = selected
+    }
+    //全選功能
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
+    const allCheck = (selected) => {
+        cartList.value.forEach(item => item.selected = selected)
+    }
+
     return {
         cartList,
         addCart,
         delCart,
         allCount,
-        allPrice
+        allPrice,
+        singleCheck,
+        isAll,
+        allCheck
     }
 }, {
     persist: true
