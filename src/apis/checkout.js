4a8c27a17c6346a419100da5e491433bf13ec657
ziya <ziyawang0806@gmail.com>
2024-04-01 01:12:46 +0800

    支付基礎數據

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -1,7 +1,17 @@
 import request from '@/utils/http'
 
-export const getCheckInfoAPI = ()=>{
+//獲取詳情
+export const getCheckInfoAPI = () => {
     return request({
-        url:'/member/order/pre'
+        url: '/member/order/pre'
+    })
+}
+
+//創建訂單
+export const createOrderAPI = (data) => {
+    return request({
+        url: '/member/order',
+        method: 'POST',
+        data
     })
 }
--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,10 @@
 <script setup>
-import {getCheckInfoAPI} from '@/apis/checkout'
+import {getCheckInfoAPI,createOrderAPI} from '@/apis/checkout'
+import {useRouter} from 'vue-router'
 import  {ref,onMounted} from 'vue'
+import { useCartStore } from '@/stores/cartStore';
+const cartStore = useCartStore()
+const router = useRouter()
 const checkInfo = ref({})  // 订单对象
 const curAddress = ref({})
 const getCheckInfo = async()=>{
@@ -22,7 +26,32 @@ const switchAddress = (item)=>{
 const confirm = ()=>{
   curAddress.value = activeAddress.value
   showDialog.value = false
-  activeAddress.value={}
+  activeAddress.value = {}
+}
+//創建訂單
+const createOrder = async ()=>{
+  const res = await createOrderAPI({
+    deliveryTimeType:1,
+    payType:1,
+    payChannel:1,
+    buyerMessage:'',
+    goods:checkInfo.value.goods.map(item=>{
+      return {
+        skuId:item.skuId,
+        count:item.count
+      }
+    }),
+    addressId:curAddress.value.id
+  })
+  const orderId = res.result.id
+  router.push({
+    path:'/pay',
+    query:{
+      id:orderId
+    }
+  })
+  //更新購物車
+  cartStore.updateNewList()
 }
 
 </script>
@@ -119,7 +148,7 @@ const confirm = ()=>{
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button @click="createOrder" type="primary" size="large">提交订单</el-button>
         </div>
       </div>
     </div>
