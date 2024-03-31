6b2c383aa21699ce2ce8ae8b1269b3eeb51b9a60
ziya <ziyawang0806@gmail.com>
2024-04-01 01:22:04 +0800

    支付功能

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -10,6 +10,13 @@ const getPayInfo = async () => {
     payInfo.value = res.result
 }
 onMounted(() => getPayInfo())
+
+//跳轉支付，攜帶訂單id及回調地址跳轉到支付地址
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
 
 
